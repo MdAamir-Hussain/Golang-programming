d19ffa577562a97eb2c1b9e3688f6092613704dc
Md Aamir Hussain <mdaamirhussain319@gmail.com>
2023-09-02 18:55:42 +0530

    variable

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello form go programming language!!!")
+	// fmt.Println("hello form go programming language!!!")
+	var username string = "aamirhussain"
+	fmt.Println(username)
+	fmt.Printf("variable is of type: %T \n", username)
 }
